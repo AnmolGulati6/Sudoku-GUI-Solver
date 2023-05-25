fa1eb97fdb45c6509542feeff1ce84fda91681a0
Anmol Gulati <61374198+AnmolGulati6@users.noreply.github.com>
2023-05-24 20:05:49 -0700

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,187 +1,333 @@
 # Written by Anmol Gulati
+import pygame
+import time
+import solve
 
-import copy
-from random import randint, shuffle
-
-# Initialise empty 9x9 board
-
-grid = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+pygame.font.init()
 
 
-# A backtracking function to check all possible combinations of numbers until a solution is found
-def solveGrid(board):
-    global counter
-    counter = 0
-    # Find next empty cell
-    for i in range(0, 81):
-        row = i // 9
-        col = i % 9
-        if board[row][col] == 0:
-            for value in range(1, 10):
-                # Check that this value has not already be used on this row
-                if not (value in board[row]):
-                    # Check that this value has not already be used on this column
-                    if not value in (
-                            board[0][col], board[1][col], board[2][col], board[3][col], board[4][col], board[5][col],
-                            board[6][col],
-                            board[7][col], board[8][col]):
-                        # Identify which of the 9 squares we are working on
-                        square = []
-                        if row < 3:
-                            if col < 3:
-                                square = [board[i][0:3] for i in range(0, 3)]
-                            elif col < 6:
-                                square = [board[i][3:6] for i in range(0, 3)]
-                            else:
-                                square = [board[i][6:9] for i in range(0, 3)]
-                        elif row < 6:
-                            if col < 3:
-                                square = [board[i][0:3] for i in range(3, 6)]
-                            elif col < 6:
-                                square = [board[i][3:6] for i in range(3, 6)]
-                            else:
-                                square = [board[i][6:9] for i in range(3, 6)]
-                        else:
-                            if col < 3:
-                                square = [board[i][0:3] for i in range(6, 9)]
-                            elif col < 6:
-                                square = [board[i][3:6] for i in range(6, 9)]
-                            else:
-                                square = [board[i][6:9] for i in range(6, 9)]
-                        # Check that this value has not already be used on this 3x3 square
-                        if not value in (square[0] + square[1] + square[2]):
-                            board[row][col] = value
-                            if checkGrid(board):
-                                counter += 1
-                                break
-                            else:
-                                if solveGrid(board):
-                                    return True
-            break
-    grid[row][col] = 0
+class Grid:
+    board = solve.grid  # Assuming solve.grid is defined and accessible
 
+    def __init__(self, rows, cols, width, height, win):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.update()
+        self.selected = None
+        self.win = win
 
-# Formats the sudoku board for console
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+    def update(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+    def place(self, val):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set(val)
+            self.update()
 
-            if j == 8:
-                print(bo[i][j])
+            if valid(self.model, val, (row, col)) and self.solution():
+                return True
             else:
-                print(str(bo[i][j]) + " ", end="")
-
-
-# This function returns true if the grid is full
-def checkGrid(board):
-    for row in range(0, 9):
-        for col in range(0, 9):
-            if board[row][col] == 0:
+                self.cubes[row][col].set(0)
+                self.cubes[row][col].set_temp(0)
+                self.update()
                 return False
+
+    def sketch(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set_temp(val)
+
+    def draw(self):
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows + 1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(self.win, (0, 0, 0), (0, i * gap), (self.width, i * gap), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(self.win)
+
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_temp(0)
+
+    def click(self, pos):
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return int(y), int(x)
+        else:
+            return None
+
+    def finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+    # Alternate way of solving the sudoku board using same algorithm
+    def solution(self):
+        find = find_empty(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if valid(self.model, i, (row, col)):
+                self.model[row][col] = i
+
+                if self.solution():
+                    return True
+
+                self.model[row][col] = 0
+
+        return False
+
+    def solve_gui(self):
+        self.update()
+        find = find_empty(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if valid(self.model, i, (row, col)):
+                self.model[row][col] = i
+                self.cubes[row][col].set(i)
+                self.cubes[row][col].draw_change(self.win, True)
+                self.update()
+                pygame.display.update()
+                pygame.time.delay(100)
+
+                if self.solve_gui():
+                    return True
+
+                self.model[row][col] = 0
+                self.cubes[row][col].set(0)
+                self.update()
+                self.cubes[row][col].draw_change(self.win, False)
+                pygame.display.update()
+                pygame.time.delay(100)
+
+        return False
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.temp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.temp != 0 and self.value == 0:
+            text = fnt.render(str(self.temp), 1, (128, 128, 128))
+            win.blit(text, (x + 5, y + 5))
+        elif not (self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def draw_change(self, win, g=True):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
+
+        text = fnt.render(str(self.value), 1, (0, 0, 0))
+        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+    def set_temp(self, val):
+        self.temp = val
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return i, j  # row, col
+
+    return None
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
     return True
 
 
-numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def redraw_window(win, board, time, strikes):
+    win.fill((255, 255, 255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0, 0, 0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw()
 
 
-# backtracking alg to check all possible combinations of numbers until a solution is found
-def fillGrid(grid):
-    global counter
-    # Find next empty cell
-    for i in range(0, 81):
-        row = i // 9
-        col = i % 9
-        if grid[row][col] == 0:
-            shuffle(numberList)
-            for value in numberList:
-                # Check that this value has not already be used on this row
-                if not (value in grid[row]):
-                    # Check that this value has not already be used on this column
-                    if not value in (
-                            grid[0][col], grid[1][col], grid[2][col], grid[3][col], grid[4][col], grid[5][col],
-                            grid[6][col],
-                            grid[7][col], grid[8][col]):
-                        # Identify which of the 9 squares we are working on
-                        square = []
-                        if row < 3:
-                            if col < 3:
-                                square = [grid[i][0:3] for i in range(0, 3)]
-                            elif col < 6:
-                                square = [grid[i][3:6] for i in range(0, 3)]
-                            else:
-                                square = [grid[i][6:9] for i in range(0, 3)]
-                        elif row < 6:
-                            if col < 3:
-                                square = [grid[i][0:3] for i in range(3, 6)]
-                            elif col < 6:
-                                square = [grid[i][3:6] for i in range(3, 6)]
-                            else:
-                                square = [grid[i][6:9] for i in range(3, 6)]
+def format_time(secs):
+    sec = secs % 60
+    minute = secs // 60
+    hour = minute // 60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
+
+
+def main():
+    win = pygame.display.set_mode((625, 625))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9, 9, 540, 540, win)
+    key = None
+    run = True
+    start = time.time()
+    strikes = 0
+    while run:
+
+        play_time = round(time.time() - start)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+                if event.key == pygame.K_KP1:
+                    key = 1
+                if event.key == pygame.K_KP2:
+                    key = 2
+                if event.key == pygame.K_KP3:
+                    key = 3
+                if event.key == pygame.K_KP4:
+                    key = 4
+                if event.key == pygame.K_KP5:
+                    key = 5
+                if event.key == pygame.K_KP6:
+                    key = 6
+                if event.key == pygame.K_KP7:
+                    key = 7
+                if event.key == pygame.K_KP8:
+                    key = 8
+                if event.key == pygame.K_KP9:
+                    key = 9
+                if event.key == pygame.K_DELETE:
+                    board.clear()
+                    key = None
+
+                if event.key == pygame.K_SPACE:
+                    board.solve_gui()
+
+                if event.key == pygame.K_RETURN:
+                    i, j = board.selected
+                    if board.cubes[i][j].temp != 0:
+                        if board.place(board.cubes[i][j].temp):
+                            print("Success")
                         else:
-                            if col < 3:
-                                square = [grid[i][0:3] for i in range(6, 9)]
-                            elif col < 6:
-                                square = [grid[i][3:6] for i in range(6, 9)]
-                            else:
-                                square = [grid[i][6:9] for i in range(6, 9)]
-                        # Check that this value has not already be used on this 3x3 square
-                        if not value in (square[0] + square[1] + square[2]):
-                            grid[row][col] = value
-                            if checkGrid(grid):
-                                return True
-                            else:
-                                if fillGrid(grid):
-                                    return True
-            break
-    grid[row][col] = 0
+                            print("Wrong")
+                            strikes += 1
+                        key = None
+
+                        if board.finished():
+                            print("Game over")
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+                    key = None
+
+        if board.selected and key != None:
+            board.sketch(key)
+
+        redraw_window(win, board, play_time, strikes)
+        pygame.display.update()
 
 
-fillGrid(grid)
-# store a copy of the solution
-solution = copy.deepcopy(grid)
-
-# increase attempts to increase difficulty
-attempts = 5
-counter = 1
-while attempts > 0:
-    # Select random cell that is not already empty
-    row = randint(0, 8)
-    col = randint(0, 8)
-    while grid[row][col] == 0:
-        row = randint(0, 8)
-        col = randint(0, 8)
-    # store its cell value in case we need to put it back
-    backup = grid[row][col]
-    grid[row][col] = 0
-
-    # Take a full copy of the grid
-    copyGrid = []
-    for r in range(0, 9):
-        copyGrid.append([])
-        for c in range(0, 9):
-            copyGrid[r].append(grid[r][c])
-    counter = 0
-    solveGrid(copyGrid)
-    if counter != 1:
-        grid[row][col] = backup
-        attempts -= 1
-print("Problem: ")
-print_board(grid)
-print("_____________________")
-print("Solution")
-print_board(solution)
+main()
+pygame.quit()
